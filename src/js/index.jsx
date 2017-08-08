ef042032b83f93c0984eae7e56bbfdff60f3a9e6
kothadiaab <kothadiaab@gmail.com>
2017-08-07 20:54:23 -0400

    added babel and index content

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,1 +1,10 @@
-alert('Boston Durga Bari');
+import React from 'react';
+import {render} from 'react-dom';
+
+class IndexView extends React.Component {
+  render() {
+    return <h1>Welcome to Durgabari 2017</h1>
+  }
+}
+
+render(<IndexView />, document.getElementById('site_wrapper'));
